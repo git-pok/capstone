0cee052e5a638650f907a34a6227274baca58842
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2023-08-02 12:53:26 -0400

    Edited app.js in /backend: defined test route.

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,14 @@
 const express = require("express");
 const app = express();
+
+/**
+ * "/"
+ * Test route to verify request works.
+ */
+app.get("/", (req, res) => {
+    return res.status(200).json({
+        msg: "HOME ROUTE!"
+    });
+});
+
 module.exports = app;
