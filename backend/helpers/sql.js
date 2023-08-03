8e9a8300acb9b3346f7253f579d6a9584a021b33
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2023-08-03 10:36:48 -0400

    Edited /backend/helpers/sql.js: refactored genInsertSql.

--- backend/helpers/sql.js
+++ backend/helpers/sql.js
@@ -1,19 +1,26 @@
 const ExpressError = require("../models/error.js");
 
-function makeSqlNoSanitize (table, data, sqlExp) {
+function genInsertSql (table, data, sqlExp, returning = false) {
     try {
-        let sql = `${sqlExp} ${table}`;
+        const dataArray = Object.entries(data);
+        const sqlQryEx = [sqlExp, table];
         const columns = [];
         const values = [];
-        for (let prop in data) {
-            columns.push(prop);
-            values.push(data[prop]);
-        }
-        sql = `${sql} (${columns}) VALUES (${values})`;
-        return sql;
+        const parametizers = [];
+        dataArray.forEach((data, idx) => {
+            columns.push(data[0]);
+            values.push(data[1]);
+            parametizers.push(`$${idx + 1}`);
+        })
+        let sql = `${sqlQryEx.join(" ")} (${columns.join(", ")}) VALUES (${parametizers.join(", ")})`;
+        sql = !returning ? `${sql},` : `${sql} RETURNING ${returning.join(", ")}`;
+        return {
+            sql,
+            values
+        };
     } catch (err) {
         throw new ExpressError(400, "Bad request!");
     }
 }
 
-module.exports = { makeSqlNoSanitize };
+module.exports = { genInsertSql };
