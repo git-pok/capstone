299f02a7c7a14d43ca5069589fc5ee3da01527e1
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2023-09-09 20:10:53 -0400

    Completed more frontend tests.

--- frontend/savour/src/__tests__/SavourApp.test.js
+++ frontend/savour/src/__tests__/SavourApp.test.js
@@ -2,6 +2,16 @@ import SavourApp from '../SavourApp.js';
 import { render, fireEvent } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
+afterAll(() => {
+  const { getByText } = render(
+    <MemoryRouter intitialEntries={["/"]}>
+      <SavourApp />
+    </MemoryRouter>
+  );
+  const logoutBtn = getByText("LOG OUT");
+  fireEvent.click(logoutBtn);
+});
+
 test("renders SavourApp without crashing", () => {
   render(<MemoryRouter><SavourApp /></MemoryRouter>);
 });
@@ -35,4 +45,38 @@ test("login form submission", async () => {
   fireEvent.click(homeBtn);
   const welcome = await findByText("Welcome to Savour – The recipe app");
   expect(welcome).toBeInTheDocument();
+});
+
+test("home page to shoppinglists page", async () => {
+  const { debug, findByText, getByText } = render(
+                                  <MemoryRouter intitialEntries={["/"]}>
+                                    <SavourApp />
+                                  </MemoryRouter>
+                                );
+
+  const welcome = getByText("Welcome to Savour – The recipe app");
+  expect(welcome).toBeInTheDocument();
+  const shplistsBtn = getByText("SHOPLISTS");
+  fireEvent.click(shplistsBtn);
+  const shplistsText = await findByText("drisqol's Shoppinglists");
+  expect(shplistsText).toBeInTheDocument();
+});
+
+test("profile page to contact page", async () => {
+  const { debug, findByText, getByText } = render(
+                                  <MemoryRouter intitialEntries={["/"]}>
+                                    <SavourApp />
+                                  </MemoryRouter>
+                                );
+
+  const welcome = getByText("Welcome to Savour – The recipe app");
+  expect(welcome).toBeInTheDocument();
+  const profileBtn = getByText("PROFILE");
+  fireEvent.click(profileBtn);
+  const profileText = await findByText("drisqol Details");
+  expect(profileText).toBeInTheDocument();
+  const contactBtn = getByText("CONTACT");
+  fireEvent.click(contactBtn);
+  const contactText = await findByText("App Creator and Developer Information");
+  expect(contactText).toBeInTheDocument();
 });
